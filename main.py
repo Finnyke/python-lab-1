d816db9e39804dfe5946eb24397d373901e1a6ff
Maxim Pupykin <58301278+Finnyke@users.noreply.github.com>
2020-10-28 16:58:56 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,24 @@ import re
 correct_numbers = []
 
 with open("phone-numbers", "r") as numbers:
-    contents = numbers.read()
-    strings_1 = re.findall(r"\D9\d{9}\D", contents)
-    strings_2 = re.findall(r"\D8\d{10}\D", contents)
-    strings_3 = re.findall(r"\D7\d{10}\D", contents)
-
-for string in strings_1:
-    correct_numbers.append(string[1:11])
-for string in strings_2:
-    correct_numbers.append(string[1:12])
-for string in strings_3:
-    correct_numbers.append(string[1:12])
+    for number in numbers:
+        if re.fullmatch(r"9\d{2}-?\d{3}-?\d{2}-?\d{2}\D?", number):
+            correct_numbers.append(number)
+        elif re.fullmatch(r"\(9\d{2}\)\d{3}-?\d{2}-?\d{2}\D?", number):
+            correct_numbers.append(number)
+        elif re.fullmatch(r"8\(\d{3}\)\d{3}-?\d{2}-?\d{2}\D?", number):
+            correct_numbers.append(number)
+        elif re.fullmatch(r"8-?\d{3}-?\d{3}-?\d{2}-?\d{2}\D?", number):
+            correct_numbers.append(number)
+        elif re.fullmatch(r"\+?7\(\d{3}\)\d{3}-?\d{2}-?\d{2}\D?", number):
+            correct_numbers.append(number)
+        elif re.fullmatch(r"\+?7-?\d{3}-?\d{3}-?\d{2}-?\d{2}\D?", number):
+            correct_numbers.append(number)
 
 with open("correct-numbers", "w") as numbers:
     for number in correct_numbers:
-        numbers.write(number + "\n")
+        if number[len(number)-1] == '\n':
+            numbers.write(number)
+        else:
+            numbers.write(number + "\n")
+
