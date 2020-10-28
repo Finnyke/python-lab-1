60c9b3738adc024dbeb96380e4fd16b51d837e7a
Maxim Pupykin <58301278+Finnyke@users.noreply.github.com>
2020-10-28 20:20:33 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ correct_numbers = []
 
 with open("phone-numbers.txt", "r") as numbers:
     for number in numbers:
-        if re.fullmatch(r"9\d{2}-?\d{3}-?\d{2}-?\d{2}\D?", number):
+        if re.fullmatch(r"9\d{2}(?:-\d{3}-\d{2}-|\d{5})\d{2}\D?", number):
             correct_numbers.append(number)
-        elif re.fullmatch(r"\(9\d{2}\)\d{3}-?\d{2}-?\d{2}\D?", number):
+        elif re.fullmatch(r"\(9\d{2}\)\d{3}(?:-\d{2}-|\d{2})\d{2}\D?", number):
             correct_numbers.append(number)
-        elif re.fullmatch(r"8\(\d{3}\)\d{3}-?\d{2}-?\d{2}\D?", number):
+        elif re.fullmatch(r"8\(\d{3}\)\d{3}(?:-\d{2}-|\d{2})\d{2}\D?", number):
             correct_numbers.append(number)
-        elif re.fullmatch(r"8-?\d{3}-?\d{3}-?\d{2}-?\d{2}\D?", number):
+        elif re.fullmatch(r"8(?:-\d{3}-\d{3}-\d{2}-|\d{8})\d{2}\D?", number):
             correct_numbers.append(number)
-        elif re.fullmatch(r"\+?7\(\d{3}\)\d{3}-?\d{2}-?\d{2}\D?", number):
+        elif re.fullmatch(r"\+?7\(\d{3}\)\d{3}(?:-\d{2}-|\d{2})\d{2}\D?", number):
             correct_numbers.append(number)
-        elif re.fullmatch(r"\+?7-?\d{3}-?\d{3}-?\d{2}-?\d{2}\D?", number):
+        elif re.fullmatch(r"\+?7(?:-\d{3}-\d{3}-\d{2}-|\d{8})\d{2}\D?", number):
             correct_numbers.append(number)
 
 with open("correct-numbers.txt", "w") as numbers:
