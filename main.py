290c1c5186b811070b6a13e4bf9fab21f6d4598d
Maxim Pupykin <58301278+Finnyke@users.noreply.github.com>
2020-10-28 21:03:00 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ import re
 correct_numbers = []
 
 with open("phone-numbers.txt", "r") as numbers:
-    for number in numbers:
-        if re.fullmatch(r"9\d{2}(?:-\d{3}-\d{2}-|\d{5})\d{2}\D?", number):
-            correct_numbers.append(number)
-        elif re.fullmatch(r"\(9\d{2}\)\d{3}(?:-\d{2}-|\d{2})\d{2}\D?", number):
-            correct_numbers.append(number)
-        elif re.fullmatch(r"8\(\d{3}\)\d{3}(?:-\d{2}-|\d{2})\d{2}\D?", number):
-            correct_numbers.append(number)
-        elif re.fullmatch(r"8(?:-\d{3}-\d{3}-\d{2}-|\d{8})\d{2}\D?", number):
-            correct_numbers.append(number)
-        elif re.fullmatch(r"\+?7\(\d{3}\)\d{3}(?:-\d{2}-|\d{2})\d{2}\D?", number):
-            correct_numbers.append(number)
-        elif re.fullmatch(r"\+?7(?:-\d{3}-\d{3}-\d{2}-|\d{8})\d{2}\D?", number):
-            correct_numbers.append(number)
+    input_string = numbers.read()
+
+input_numbers = re.split(r"[^(?:0-9|\+|\-|\(|\))]", input_string)
+
+for number in input_numbers:
+    if re.fullmatch(r"9\d{2}(?:-\d{3}-\d{2}-|\d{5})\d{2}\D?", number):
+        correct_numbers.append(number)
+    elif re.fullmatch(r"\(9\d{2}\)\d{3}(?:-\d{2}-|\d{2})\d{2}\D?", number):
+        correct_numbers.append(number)
+    elif re.fullmatch(r"8\(\d{3}\)\d{3}(?:-\d{2}-|\d{2})\d{2}\D?", number):
+        correct_numbers.append(number)
+    elif re.fullmatch(r"8(?:-\d{3}-\d{3}-\d{2}-|\d{8})\d{2}\D?", number):
+        correct_numbers.append(number)
+    elif re.fullmatch(r"\+?7\(\d{3}\)\d{3}(?:-\d{2}-|\d{2})\d{2}\D?", number):
+        correct_numbers.append(number)
+    elif re.fullmatch(r"\+?7(?:-\d{3}-\d{3}-\d{2}-|\d{8})\d{2}\D?", number):
+        correct_numbers.append(number)
 
 with open("correct-numbers.txt", "w") as numbers:
     for number in correct_numbers:
-        numbers.write(number)
+        numbers.write(number + "\n")
